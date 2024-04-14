dedccf249756557fdcacfe15d32eccee04104f6c
Omelette 蛋卷 <phildu202@yahoo.com>
2024-04-14 08:19:41 -0400

    Refactor exception handling to main

--- src/modules/query.py
+++ src/modules/query.py
@@ -7,6 +7,10 @@ from modules import visualize
 
 
 class Query:
+    """
+    Singleton class to handle query requests on a flow field.
+    Currently only save results to disk in numpy format.
+    """
     _instance = None
 
     save_results = True
@@ -20,41 +24,69 @@ class Query:
         self.field = field
 
     def handle_request(self, request: str, format="string"):
+        """
+        Handle query request on flow field.
+        Supports two modes: meshgrid and points.
+        For points, it currrently uses single point meshgrid calculation.
+
+        Parameters
+        ----------
+        request : str
+            Query request string or file name.
+        format : str, optional
+            Format of request, by default "string"
+
+        Returns
+        -------
+        response : str
+            Response message.
+
+        """
+        # Parse request
         if format == "file":
             request = file_io.read("queries", request, format="json")
         else:
             try:
                 request: dict = json.loads(request)
             except Exception as e:
-                return f"Invalid query request string: {e}"
+                raise Exception(f"Invalid query request string: {e}")
 
-        mode: str = request.get("mode", None)
+        mode: str = request.get("mode", "INVALID")
         params: dict = request.get("params", None)
         if not isinstance(params, dict):
-            return "Invalid request parameters"
+            raise TypeError("Invalid request parameters")
 
+        # File name for saving results
+        current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"{self.field.name}_{mode}_{current_time}"
+
+        # Response message
+        response = f"Velocity calculation complete (mode: {mode})."
+
+        # Handle meshgrid request
         if mode == "meshgrid":
+            # Extract mesh parameters
             low_bounds = params.get("low_bounds", None)
             high_bounds = params.get("high_bounds", None)
 
             keys = ["low_bounds", "high_bounds", "step_size", "chunk_size", "t"]
             args = {key: params[key] for key in keys if key in params}
 
+            # Calculate velocity in meshgrid
             try:
-                vel = self.field.sum_vel_mesh(
-                    **args,
-                )
+                vel = self.field.sum_vel_mesh(**args)
             except Exception as e:
-                return f"Error calculating velocity in meshgrid: {e}"
+                raise Exception(f"Error calculating velocity in meshgrid: {e}")
 
+            # Save raw results to disk
             if isinstance(vel, np.ndarray) and self.save_results:
                 try:
-                    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-                    filename = f"{self.field.name}_meshgrid_{current_time}"
                     file_io.write("results", filename, vel, format="npy")
+                    response += f"\nRaw result saved to results/{filename}.npy"
                 except Exception as e:
-                    return f"Error saving result: {e}"
+                    raise Exception(f"Error saving raw result: {e}")
 
+            # Plot meshgrid if requested
             plot: dict = request.get("plot", None)
             if plot is not None:
                 try:
@@ -65,18 +97,26 @@ class Query:
                         **plot,
                     )
                 except Exception as e:
-                    return f"Error plotting meshgrid: {e}"
-                return fig
-            else:
-                return "Meshgrid velocity calculation complete"
+                    raise Exception(f"Error plotting meshgrid: {e}")
 
-        if request.get("mode") == "points":
+                # Save plot to disk
+                try:
+                    file_io.write("plots", filename, fig, format="png")
+                    response += f"\nPlot saved to plots/{filename}.png"
+                except Exception as e:
+                    raise Exception(f"Error saving plot: {e}")
+            return response
+
+        # Handle points request
+        elif mode == "points":
+            # Extract points coordinates
             coords: list = params.get("coords", None)
             if coords is None:
                 coords = [[0, 0, 0]]
             if not isinstance(coords, list) or len(coords) == 0:
-                return "Invalid request parameters, coords must be a list of 3D points"
+                raise TypeError("Invalid request parameters, coords must be a list of 3D points")
 
+            # Calculate velocity at each point
             velocities = np.zeros((len(coords), 3))
             try:
                 for i, coord in enumerate(coords):
@@ -84,13 +124,17 @@ class Query:
                         low_bounds=coord, high_bounds=coord, t=params.get("t", 0)
                     )
             except Exception as e:
-                return f"Error calculating velocity at points: {e}"
+                raise Exception(f"Error calculating velocity at points: {e}")
+
+            # Save results to disk
             if isinstance(velocities, np.ndarray):
                 try:
-                    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-                    filename = f"{self.field.name}_points_{current_time}"
                     file_io.write("results", filename, velocities, format="npy")
+                    response += f"\nRaw result saved to results/{filename}.npy"
                 except Exception as e:
-                    return f"Error saving result: {e}"
-            return "Points velocity calculation complete"
-        return "Invalid request mode"
+                    raise Exception(f"Error saving result: {e}")
+            return response
+
+        # No valid mode found
+        else:
+            raise Exception("Invalid request mode")
